ffbc4f1b771dac6baee00bc69a7fe4938f2b4d06
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-25 22:01:04 +0100

    Update App file

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_player/models/episode.dart';
+import 'package:podcast_player/models/podcast.dart';
+import 'package:podcast_player/scoped_models/scoped_playlist.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final playlistManager = ScopedPlaylist();
+
+    final podcasts = Podcast.fetchAll();
+    for (Podcast p in podcasts) {
+      for (Episode e in p.episodes) {
+        playlistManager.addEpisode(e);
+      }
+    }
+
     return MaterialApp(
       home: Playlist(),
     );
