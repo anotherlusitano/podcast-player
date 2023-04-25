76355de57e796e06e04c57076f1bec43598e4281
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-25 21:57:29 +0100

    Add scoped playlist

---
+++ lib/scoped_models/scoped_playlist.dart
@@ -0,0 +1,20 @@
+import 'package:scoped_model/scoped_model.dart';
+import '../models/episode.dart';
+
+class ScopedPlaylist extends Model {
+  List<Episode> episodes = [];
+  Episode selectedEpisode;
+
+  addEpisode(Episode episode) {
+    episodes.add(episode);
+    notifyListeners();
+  }
+
+  setSelectedEpisode(Episode e) {
+    if (selectedEpisode != null) {
+      selectedEpisode.isPlaying = false;
+    }
+    selectedEpisode = e;
+    selectedEpisode.isPlaying = true;
+  }
+}
